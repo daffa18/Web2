fd8a7e9a0e509f1a0297c69bb0c0789d9d637992
Daffa Aulia Isham <80708687+daffa18@users.noreply.github.com>
2024-02-29 22:55:52 +0700

    Add files via upload

---
+++ Pertemuan 1/contoh_2.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test Penyisipan PHP Pada HTML</title>
+</head>
+<body>
+    <h2>1st Thing 1st, Silakan identifikasikan diri Anda!</h2>
+    <hr color="blue" size="4" width="60%" align="left">
+<?php 
+//BerikutiniadalahkodePHPyangdisisipkan
+    Print"<b><fontface='arial'size=5color='sienna'>
+    Momentum tahun baru memang paling enak untuk di jadikan turning perbaikan diri<br>";
+    print"Buthow? Begitu banyak kebiasaan buruk, begitu banyak masalah, begitu banyak tujuan,<br>";
+    echo"begitu banyak mimpi,bla...bla...bla...Stop! tentukan prioritas...firstthing first!</font></b>";
+
+?>
+    <h3>Selamat belajar PHP</h3>
+<?php 
+    print"<pre>";
+    print"<center><font size=14 color='magenta'>BIODATA</font></center>";
+    echo"<hr size=4 width=60% color='blue'>";
+    print"<font size=12 color='red'> Nama      :Daffa Aulia Isham</font><br>";
+    print"<font size=12 color='blue'> Kelas     :06TPLM005</font><br>"; 
+    print"<font size=12 color='green'> Jurusan   :Teknik Informatika</font><br>"; 
+    print"<font size=12 color='purple'> Hoby      :Traveling </font><br></pre>";
+    echo"<hr size=4 width=60% color='blue'>";
+
+
+?>
+</body>
+</html>
