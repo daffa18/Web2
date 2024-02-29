eab1dfde9415f69a10d729e8ac325f9a10851aeb
Daffa Aulia Isham <80708687+daffa18@users.noreply.github.com>
2024-02-29 23:05:20 +0700

    Add files via upload

---
+++ Pertemuan 1/latihansoal_1.php
@@ -0,0 +1,14 @@
+//Daffa Aulia isham
+//06TPLM005
+
+<?php 
+    print"<pre>";
+    print"<center><font size=14 color='red'><b>BIODATA<b></font></center>";
+    print"<font size=12 color='blue'> Nama :   <b>Achmad Riki<b></font><br>";
+    print"<font size=12 color='blue'> Email :  ariki@yahoo.com</font><br>"; 
+    print"<font size=12 color='blue'> Jurusan :Teknik Informatika</font><br>"; 
+    print"<font size=12 color='blue'> Hoby :   Traveling </font><br></pre>";
+
+
+
+?>
