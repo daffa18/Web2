6cda63ac5d1134403bab1eace4519f14a211c7ef
Daffa Aulia Isham <80708687+daffa18@users.noreply.github.com>
2024-02-29 22:40:02 +0700

    Add files via upload

---
+++ Pertemuan 1/contoh_2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test Penyisipan PHP Pada HTML</title>
+</head>
+<body>
+    <h2>1st Thing 1st, Silakan identifikasikan diri Anda!</h2>
+    <hr color="blue" size="4" width="60%" align="left">
+<?php 
+//BerikutiniadalahkodePHPyangdisisipkan
+Print"<b><fontface='arial'size=5color='sienna'>Momentum tahun baru
+memangpalingenak untuk di jadikan turning perbaikan diri<br>";
+print"Buthow? Begitu banyak kebiasaan buruk, begitu banyak masalah,
+begitu banyak tujuan,<br>";
+echo"begitu banyak mimpi,bla...bla...bla...Stop! tentukan prioritas...firstthing
+first!</font></b>";
+
+?>
+</html>
